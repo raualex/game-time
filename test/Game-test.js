bf9e0481e259a19f64d19fe8d5971a67f7e1514d
Alexander Rau <alexanderrau86@yahoo.com>
2018-08-15 22:26:46 -0600

    Write and pass basic functionality tests

--- test/Game-test.js
+++ test/Game-test.js
@@ -22,19 +22,40 @@ describe('Game', () => {
     assert.isTrue(game.gameOver);
   });
 
-  it.skip('should detect if snake collides with food', () => {
+  it('should detect if snake collides with food', () => {
+    const game = new Game(ctx);
     const snake = game.snake;
     const food = game.food;
-    
+
     snake.x = food.x;
+    snake.y = food.y;
 
-    let colliding = snake.isCollidingWith(food)
+    let colliding = snake.isCollidingWith(food);
 
-    assert.equal(colliding, true)
+    assert.equal(colliding, true);
 
   })
-  it.skip('should end game', () => {})
-  it.skip('should collide with walls', () => {})
-  it.skip('should be able to move', () => {})
-  it.skip('should be able to changeDirection', () => {})
+
+  it('should be able to move', () => {
+    const game = new Game(ctx);
+    const snake = game.snake;
+
+    assert.equal(snake.dx, 1 || snake.dy, 1);
+    assert.equal(snake.dxv, 1 || snake.dyv, 1);    
+  })
+
+  it('should be able to changeDirection', () => {
+    const game = new Game(ctx);
+    const snake = game.snake;
+    const fakeKeyPress = { key: 'ArrowLeft' };
+
+    game.handleKeyPress(fakeKeyPress);
+    assert.equal(snake.dx, -1);
+  })
+
 });
+
+
+
+
+
